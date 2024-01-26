7a74bbb19d689b227c1e2b50e00896672fc7c54b
Nadav Galili <nadavg@mobile-brain.net>
2024-01-26 12:06:26 +0200

    Refactor scroll event listener and add current year to footer

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 document.addEventListener("scroll", function () {
-    const stickyButtons = document.querySelector(".sticky-buttons"); // Assuming there's only one .sticky-buttons element
+  const stickyButtons = document.querySelector(".sticky-buttons"); // Assuming there's only one .sticky-buttons element
 
-    const scrollPosition = window.scrollY;
-    const elements = document.querySelectorAll(".col-md-4.col-sm-12");
+  const scrollPosition = window.scrollY;
+  const elements = document.querySelectorAll(".col-md-4.col-sm-12");
 
-    const windowHeight = window.innerHeight;
-    const documentHeight = document.documentElement.scrollHeight;
+  const windowHeight = window.innerHeight;
+  const documentHeight = document.documentElement.scrollHeight;
 
-    // Show the elements when scroll position is greater than 100 pixels
-    if (scrollPosition > 100) {
-        elements.forEach((element, index) => {
-            setTimeout(() => {
-                element.style.opacity = "1";
-            }, index * 500); // 500ms delay for each element
-        });
+  // Show the elements when scroll position is greater than 100 pixels
+  if (scrollPosition > 100) {
+    elements.forEach((element, index) => {
+      setTimeout(() => {
+        element.style.opacity = "1";
+      }, index * 500); // 500ms delay for each element
+    });
 
-        // Make the buttons sticky
-        stickyButtons.style.position = "fixed";
-        stickyButtons.style.width = "100%";
-        stickyButtons.style.zIndex = "1000";
+    // Make the buttons sticky
+    stickyButtons.style.position = "fixed";
+    stickyButtons.style.width = "100%";
+    stickyButtons.style.zIndex = "1000";
 
-        // If the user has scrolled to the bottom of the page
-        if (scrollPosition + windowHeight >= documentHeight) {
-            stickyButtons.style.bottom = "0";
-        } else {
-            stickyButtons.style.bottom = "10%";
-        }
+    // If the user has scrolled to the bottom of the page
+    if (scrollPosition + windowHeight >= documentHeight) {
+      stickyButtons.style.bottom = "0";
     } else {
-        elements.forEach((element) => {
-            element.style.opacity = "0";
-        });
-
-        // Remove the sticky position from the buttons
-        stickyButtons.style.position = "static";
+      stickyButtons.style.bottom = "10%";
     }
+  } else {
+    elements.forEach((element) => {
+      element.style.opacity = "0";
+    });
+
+    // Remove the sticky position from the buttons
+    stickyButtons.style.position = "static";
+  }
 });
+
+///add current year to footer at the end of the p with id="year"
+
+const year = new Date().getFullYear();
+let pElement = document.getElementById("year");
+pElement.innerHTML = pElement.innerHTML + " " + year;
